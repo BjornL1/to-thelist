f2189682ec530ec512938759a6ee446fc31a6ae1
BjornL1 <135661785+BjornL1@users.noreply.github.com>
2024-02-16 14:03:46 +0100

    Restore views in shopping list to test heroku function

--- shopping_list/views.py
+++ shopping_list/views.py
@@ -1,3 +1,24 @@
+def shopping_list_view(request):
+    # Retrieve all shopping list items from the database
+    shopping_list = ShoppingList.objects.all()
+
+    # Render the template with the shopping list data
+    return render(request, 'shopping_list.html', {'shopping_list': shopping_list})
+
+
+def input_form_view(request):
+    # Simply render the input_form.html template
+    return render(request, 'input_form.html')
+'''
+from django.shortcuts import render, redirect
+return redirect('success_url')  # Redirect to a success page after saving
+
+---------
+from django.shortcuts import render
+from .models import ShoppingList
+
+# Create your views here.
+
 from django.shortcuts import render
 from .forms import ItemForm  # Import your ItemForm
 from .models import ShoppingList
@@ -12,28 +33,6 @@ def input_form_view(request):
 
     shopping_list = ShoppingList.objects.all()  # Retrieve all shopping list items
     return render(request, 'input_form.html', {'form': form, 'shopping_list': shopping_list})
-'''
-from django.shortcuts import render, redirect
-return redirect('success_url')  # Redirect to a success page after saving
-
----------
-from django.shortcuts import render
-from .models import ShoppingList
-
-# Create your views here.
-
-
-def shopping_list_view(request):
-    # Retrieve all shopping list items from the database
-    shopping_list = ShoppingList.objects.all()
-
-    # Render the template with the shopping list data
-    return render(request, 'shopping_list.html', {'shopping_list': shopping_list})
-
-
-def input_form_view(request):
-    # Simply render the input_form.html template
-    return render(request, 'input_form.html')
 '''
 
 
